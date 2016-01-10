d5da379b4fdd8cf0c1f8dffa7aebe945582bdd0f
Diana <Dinchik@dianas-macbook-pro.local>
2016-01-10 20:12:40 +0200

    Add tests

--- PMPLab5/PMPLab5Tests/PMPLab5Tests.m
+++ PMPLab5/PMPLab5Tests/PMPLab5Tests.m
@@ -7,9 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "StudentService.h"
 
 @interface PMPLab5Tests : XCTestCase
-
+{
+    StudentService *_service;
+}
 @end
 
 @implementation PMPLab5Tests
@@ -34,6 +37,30 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+-(void)test001_initWithMarks
+{
+    _service = [[StudentService alloc] initWithMarks:@[@(5), @(4)]];
+    XCTAssertTrue(_service.marks.count == 2, @"Object init error");
+}
+
+-(void) test002_isVaildMarks
+{
+    BOOL isValidMarks = [_service isValidMarks];
+    XCTAssertTrue(isValidMarks == YES, @"Object init error; marks are not valid");
+}
+
+-(void)test003_isPermittedToSession
+{
+    BOOL isPermittedToSessia = [_service isPermittedToSession];
+    XCTAssertTrue(isPermittedToSessia == NO, @"Method error; student is not permitted to session");
+}
+
+-(void)test004_isStipendia
+{
+    BOOL isStipendia = [_service isStipendia];
+    XCTAssertTrue(isStipendia == NO, @"Method error; student has no stipendia");
 }
 
 @end
